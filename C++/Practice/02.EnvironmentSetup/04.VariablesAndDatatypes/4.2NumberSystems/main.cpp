d55a5b4cf6f2124226ee5d1e19ec67132c97fa5e
Syed Sharukhali Farookali Naziyabegum <99337487+AI-ApeX-DeV@users.noreply.github.com>
2023-12-26 19:28:53 +0000

    added readme.md

--- C++/Practice/02.EnvironmentSetup/04.VariablesAndDatatypes/4.2NumberSystems/main.cpp
+++ C++/Practice/02.EnvironmentSetup/04.VariablesAndDatatypes/4.2NumberSystems/main.cpp
@@ -7,7 +7,6 @@ int main(){
    int number2 = 017; // Octal
    int number3 = 0x0F; // Hexadecimal
    int number4 = 0b00001111; // Binary
-
    std::cout << "number1 : " << number1 << std::endl;
    std::cout << "number2 : " << number2 << std::endl;
    std::cout << "number3 : " << number3 << std::endl;
